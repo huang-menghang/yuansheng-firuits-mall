d51327fe6b25b0c03e75ff6e977a085fce67a236
huang-menghang <449043209@qq.com>
2018-03-29 15:38:45 +0800

    基本组件

---
+++ yuansheng-firuits-mall-common/src/main/java/com/ysdevelop/page/Page.java
@@ -0,0 +1,97 @@
+package com.ysdevelop.page;
+
+import java.util.List;
+
+/**
+ * 
+ * @author HuangHang
+ * 
+ * 
+ * @date 2017/4/7
+ * 
+ *       分页接口
+ * 
+ * 
+ * @param <E>
+ * 
+ *            分页接口
+ * 
+ */
+
+public interface Page<E> extends Iterable<E> {
+
+	/**
+	 * 起始分页
+	 * 
+	 * @return
+	 * 
+	 * 
+	 */
+
+	int getFirstPageNum();
+
+	/**
+	 * 终止分页
+	 * 
+	 * @return
+	 * 
+	 * 
+	 */
+
+	int getLastPageNum();
+
+	/**
+	 * 当前页号
+	 * 
+	 * @return
+	 * 
+	 * 
+	 */
+
+	int getPageNum();
+
+	/**
+	 * 页面尺寸
+	 * 
+	 * @return
+	 * 
+	 * 
+	 */
+	int getPageSize();
+
+	/**
+	 * 页面上的内容
+	 * 
+	 * @return
+	 * 
+	 * 
+	 */
+
+	List<E> getItems();
+
+	/**
+	 * 是第一页
+	 * 
+	 * @return
+	 * 
+	 */
+	boolean isFirstPage();
+
+	/**
+	 * 到最后一页
+	 * 
+	 * @return
+	 * 
+	 */
+
+	boolean isLastPage();
+
+	/**
+	 * 是否为空
+	 * 
+	 * @return
+	 */
+
+	boolean isEmpty();
+
+}
