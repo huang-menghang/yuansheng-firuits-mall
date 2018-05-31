201dcb641895e797c06abd5e17fc0d608deeada8
zjy100109010787 <2914764147@qq.com>
2018-05-31 10:31:22 +0800

    sql语句

--- yuansheng-firuits-mall-admin/sql/install.sql
+++ yuansheng-firuits-mall-admin/sql/install.sql
@@ -30,3 +30,37 @@ CREATE TABLE `t_ys_firuits_scroll_image` (
   `updateTime` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+
+#    角色表
+CREATE TABLE `t_ys_firuits_role_permission` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `roleId` int(8) DEFAULT NULL COMMENT '角色编号',
+  `permissionId` int(8) DEFAULT NULL COMMENT '权限编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+
+#    权限表
+CREATE TABLE `t_ys_firuits_permission` (
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `permissionUrl` varchar(64) DEFAULT NULL,
+  `permissionHref` varchar(64) DEFAULT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT COMMENT '权限编码',
+  `permissionName` varchar(50) DEFAULT NULL COMMENT '权限名称',
+  `permissionLevel` int(8) DEFAULT NULL COMMENT '权限等级',
+  `permissionDescription` varchar(50) DEFAULT NULL COMMENT '权限描述',
+  `createTime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updateTime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  `parentId` int(8) DEFAULT '-1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+#    角色权限对应表
+CREATE TABLE `t_ys_firuits_role_permission` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `roleId` int(8) DEFAULT NULL COMMENT '角色编号',
+  `permissionId` int(8) DEFAULT NULL COMMENT '权限编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
